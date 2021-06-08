5b6ff82ceccbde5a0daf3336c7b0db54356d4540
Ciutina Irina <aiciutina@gmail.com>
2021-06-08 23:29:37 +0300

    Simulated Sarah's daily life

--- message.js
+++ message.js
@@ -14,6 +14,8 @@ function randomCapped(min, max){
     return random(dif) + min;
 }
 
+//This is the function used to randomize Sarah's traits
+
 function randomizeTrait(traits){
     var myTraits = [];
     for(i=0;i<5;i++){
@@ -24,6 +26,53 @@ function randomizeTrait(traits){
         myTraits.push(trait)
     }
     return myTraits;
+}
+
+//This is the function used to calculate what activity Sarah will choose
+
+function chooseActivity(person, activities, bannedActivities = []){
+    let allScores = [];
+    for(i=0;i<activities.length;i++){
+        //Two types of activities that cannot be executed are the ones that have already been executed or that Sarah is too tired to do
+        if((bannedActivities.indexOf(activities[i]) >= 0) || (person.energy < activities[i].energyScore)){
+            allScores[i] = 0;  
+        }
+        else{
+            //This is for the activities that can be completed
+            allScores[i] = randomCapped(30,60);
+            if(activities[i].isChore == true){
+                allScores[i] += 10;
+            }
+            let activityTraits = activities[i].preferredTraits;
+            let personTraits = person.traits;
+            for(j=0;j<activityTraits.length;j++){
+                for(k=0;k<personTraits.length;k++){
+                    if(activityTraits[j] == personTraits[k]){
+                        allScores[i] +=20;
+                    }
+                }
+            }
+        }
+    }
+    let maxScore = allScores[0];
+    for(i=0;i<allScores.length;i++){
+        if(allScores[i] > maxScore){
+            maxScore = allScores[i];
+        }
+    }
+    let maxIndex = allScores.indexOf(maxScore);
+    //This is when no activity is eligible for execution 
+    if(maxScore==0){
+        return null;
+    }
+    return activities[maxIndex];
+
+}
+
+//This is a checker function used for debugging
+
+function ok(message = 'OK!'){
+    console.log(message);
 }
 
 //Step 1: We will define the activities : 12 activities, 4 being chores
@@ -51,8 +100,7 @@ const chore3 = createActivity("Clean the Living Room", ['workaholic', 'neat'], 4
 const chore4 = createActivity("Pay the bills", ['workaholic'], 5, true);
 
 const traits = ['outgoing', 'nice', 'brave', 'playful', 'epicurean', 'lazy', 'sporty', 'mean', 'kleptomaniac'];
-const activities = ["Gossip with the neighbors","Throw a house party","Mix Cocktails","Play Video Games","Watch Netflix","Go Jogging","Argue with the neighbors","Steal from the local cafe",
-"Wash Dishes", "Cook a Meal", "Clean the Living Room","Pay the bills"]
+const activities = [act1, act2, act3, act4, act5, act6, act7, act8, chore1, chore2, chore3, chore4];
 
 //Step 2: Now we will work on Sarah
 
@@ -62,6 +110,26 @@ const sarah = {
     traits: randomizeTrait(traits)
 }
 
+//Step 3: Sarah's day
+
+//We need to keep track of the activities that Sarah is doing
+var completedActivities = [];
+
+while(sarah.energy>0){
+    //Sarah needs to select her activities
+    var sarahActivity = chooseActivity(sarah, activities, completedActivities);
+    //This is when Sarah cannot complete any more activities
+    if(sarahActivity == null){
+        console.log("Sarah is too tired to do anything")
+        sarah.energy = 0;
+    }
+    else{
+        sarah.energy = sarah.energy - sarahActivity.energyScore;
+        //We need to ensure that the activities are not repeated
+        completedActivities.push(sarahActivity);
+        //This code is temporary and just here for testing
+        console.log(`Sarah is executing activity ${sarahActivity.name}`);
+    }
+}
 //The area below is reserved for testing
 
-console.log(sarah.traits);
